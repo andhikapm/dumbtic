dc731e419b1930e489edfbe4364cd32af2903922
andhikapm <andhika3035790@gmail.com>
2022-12-13 18:52:56 +0700

    tic

--- Server/handlers/event.go
+++ Server/handlers/event.go
@@ -460,7 +460,7 @@ func (h *handlerEvent) CheckingEvent() {
 	const longForm = "Mon, 02 Jan 2006 15:04:00 MST"
 
 	today := time.Now().UTC()
-	today = today.Add(-time.Minute * 15)
+	today = today.Add(time.Minute * 15)
 
 	events, err := h.EventRepository.OnProgressEvent()
 	if err != nil {
