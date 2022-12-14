27016cea916e5e96ae1c3b8ce384ff9ff5c016da
andhikapm <andhika3035790@gmail.com>
2022-12-14 11:29:23 +0700

    tic

--- Server/models/transaction.go
+++ Server/models/transaction.go
@@ -6,6 +6,6 @@ type Transaction struct {
 	User     UsersResponse  `json:"user" form:"user" gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 	TicketID int            `json:"ticket_id" form:"ticket_id"`
 	Ticket   TicketResponse `json:"ticket" form:"ticket" gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
-	Price    int            `json:"price" form:"price" gorm:"type: varchar(255)"`
+	Price    int            `json:"price" form:"price"`
 	Status   string         `json:"status" form:"status" gorm:"type: varchar(255)"`
 }
